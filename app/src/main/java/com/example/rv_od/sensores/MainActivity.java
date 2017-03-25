bbd5136e5dc9dbbfc63b8e627aa67995be228985
Oscar Vivia <oscar.daniel.rv@gmail.com>
2017-03-24 18:58:11 -0600

    Initial commit

---
+++ app/src/main/java/com/example/rv_od/sensores/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.rv_od.sensores;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SensorManager sensorOscar = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorOscar.registerListener(this,sensorOscar.getDefaultSensor(Sensor.TYPE_PROXIMITY),
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
