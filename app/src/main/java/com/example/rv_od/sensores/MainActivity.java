0764d906a2565fb19acf1eba208ede506f393329
Oscar Vivia <oscar.daniel.rv@gmail.com>
2017-03-24 19:34:16 -0600

    Sensor v1.1 proximidad con color de fondo

--- app/src/main/java/com/example/rv_od/sensores/MainActivity.java
+++ app/src/main/java/com/example/rv_od/sensores/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.rv_od.sensores;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     TextView txt_encabezado;
+    RelativeLayout h_fondo;
 
 
 
@@ -20,10 +23,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        h_fondo = (RelativeLayout) findViewById(R.id.h_fondo);
         txt_encabezado = (TextView) findViewById(R.id.txt_encabezado);
-
         SensorManager sensorOscar = (SensorManager) getSystemService(SENSOR_SERVICE);
-
         sensorOscar.registerListener(this,sensorOscar.getDefaultSensor(Sensor.TYPE_PROXIMITY),//sensor. -> salen todos los tipos de sensores
                 SensorManager.SENSOR_DELAY_GAME);//SensorManager. ->es para la velocidad con la que refresca el sensor (SENSOR_DALY_ FASTER* el mas rapido GAME* para un juego NORMAL*
     }
@@ -33,6 +35,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //Toast.makeText(getBaseContext(), "Proximidad"+ sensorEvent.values[0], Toast.LENGTH_LONG).show(); //Toast son los mensajitos inseprados que salen
 
         txt_encabezado.setText("Valor: "+ sensorEvent.values[0]);
+
+        if (sensorEvent.values[0]==0)
+            h_fondo.setBackgroundColor(Color.GREEN);
+        else
+            h_fondo.setBackgroundColor(Color.CYAN);
 
     }
 
