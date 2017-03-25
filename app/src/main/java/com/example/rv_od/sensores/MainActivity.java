bd0e1505bdea66bae2d96d9d8edf1a9cc6516b91
Oscar Vivia <oscar.daniel.rv@gmail.com>
2017-03-24 19:25:06 -0600

    Sensor v1.0 proximidad

--- app/src/main/java/com/example/rv_od/sensores/MainActivity.java
+++ app/src/main/java/com/example/rv_od/sensores/MainActivity.java
@@ -6,14 +6,21 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    TextView txt_encabezado;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_encabezado = (TextView) findViewById(R.id.txt_encabezado);
 
         SensorManager sensorOscar = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -23,7 +30,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        Toast.makeText(getBaseContext(), "Proximidad"+ sensorEvent.values[0], Toast.LENGTH_LONG).show();
+        //Toast.makeText(getBaseContext(), "Proximidad"+ sensorEvent.values[0], Toast.LENGTH_LONG).show(); //Toast son los mensajitos inseprados que salen
+
+        txt_encabezado.setText("Valor: "+ sensorEvent.values[0]);
 
     }
 
